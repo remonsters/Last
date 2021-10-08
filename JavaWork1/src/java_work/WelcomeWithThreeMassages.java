889fe179d5d5d48d056c767479d5295ce3eccc7e
remonsters <643332445@qq.com>
2021-10-08 15:50:56 +0800

    JavaWork1

---
+++ JavaWork1/src/java_work/WelcomeWithThreeMassages.java
@@ -0,0 +1,12 @@
+package java_work;
+
+public class WelcomeWithThreeMassages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Programming is fun!");
+      System.out.println("FundamentaIs First");
+      System.out.println("Problem Driven");
+	}
+
+}
